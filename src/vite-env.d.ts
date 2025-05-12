c6a5071ebaa8093d9580097634dd08b53c96604b
Votre Nom <votre-email@exemple.com>
2025-05-12 21:32:27 +0200

    Ajout des définitions de types pour Vite

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface ImportMeta {
+  readonly env: {
+    readonly MODE: string;
+    readonly BASE_URL: string;
+    readonly PROD: boolean;
+    readonly DEV: boolean;
+  };
+} 
